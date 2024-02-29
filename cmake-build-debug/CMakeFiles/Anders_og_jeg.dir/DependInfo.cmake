204edd88607268c181c41b6e13e211bd7d2cab44
Anders Greve Sørensen <anders1709@gmail.com>
2024-02-29 16:40:25 +0100

    2. commit

---
+++ cmake-build-debug/CMakeFiles/Anders_og_jeg.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/AGS17/CLionProjects/machine-learning_final/main.cpp" "CMakeFiles/Anders_og_jeg.dir/main.cpp.obj" "gcc" "CMakeFiles/Anders_og_jeg.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Anders_og_jeg.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Anders_og_jeg.
+# This may be replaced when dependencies are built.
