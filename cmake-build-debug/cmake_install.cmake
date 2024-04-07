21f6132210f08a6b2c7f3c5a1802ba6a7ff9438e
Anders Greve Soerensen <anders1709@gmail.com>
2024-04-07 15:06:37 +0200

    Maybe fix shit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/steam/CLionProjects/machine-learning
+# Install script for directory: C:/Users/AGS17/Desktop/machine-learning
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/steam/CLionProjects/machine-learning/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/AGS17/Desktop/machine-learning/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
